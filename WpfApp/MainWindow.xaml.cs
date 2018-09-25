e03baf5ad3d5b3b364e883007e8889964ddd24f8
tnasergey3 <tnasergey@gmail.com>
2018-09-25 04:08:55 +0300

    25.09.2018_01

--- WpfApp/MainWindow.xaml.cs
+++ WpfApp/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using System.Data;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -26,30 +27,27 @@ namespace WpfApp
         {
             InitializeComponent();
 
-
+            // Загрузка данными из базы dataGrid_Orders
             try
             {
                 using (OrdersdbEntities db = new OrdersdbEntities())
                 {
-                    var data =
-                                            from o in db.Orders.AsEnumerable()
-                                            from l in db.List_of_order_items
-                                            from p in db.Products
-                                            where o.list_of_order_items == l.list_of_order_items__id
-                                            where l.item_product_id == p.product_id
-                                            group new { db.Orders, db.List_of_order_items, db.Products, l.item_count, p.product_price }
-                                                by new { o.orders_id, o.date, o.name_client } into g
-                                            select new
-                                            {
-                                                orders_id = g.Key.orders_id,
-                                                date = g.Key.date.ToShortDateString(),
-                                                name_client = g.Key.name_client,
-                                                totalSumOrder = g.Sum(x => x.item_count * x.product_price)
-                                            };
+                   var dataFromQuary = from o in db.Orders.AsEnumerable()
+                                       from l in db.List_of_order_items
+                                       from p in db.Products
+                                       where o.list_of_order_items == l.list_of_order_items__id
+                                       where l.item_product_id == p.product_id
+                                       group new { db.Orders, db.List_of_order_items, db.Products, l.item_count, p.product_price }
+                                       by new { o.orders_id, o.date, o.name_client } into g
+                                       select new
+                                       {
+                                           orders_id = g.Key.orders_id,
+                                           date = g.Key.date.ToShortDateString(),
+                                           name_client = g.Key.name_client,
+                                           totalSumOrder = g.Sum(x => x.item_count * x.product_price)
+                                       };
 
-                    dataGrid_Orders.ItemsSource = data;
-
-
+                    dataGrid_Orders.ItemsSource = dataFromQuary;
                 }
             }
             catch (Exception e)
@@ -58,6 +56,7 @@ namespace WpfApp
             }
         }
 
+        // Обработка события "Нажатие мыши на данные dataGrid_Orders"
         private void dataGrid_Orders_MouseUp(object sender, MouseButtonEventArgs e)
         {
             try
@@ -65,48 +64,58 @@ namespace WpfApp
                 // Проверка на то, что клик мышки будет на колонке с данными
                 if (dataGrid_Orders.CurrentCell.Column != null)
                 {
-                    // При выборе строки получение ID
+                    // При выборе строки получение ID заказа
                     object item = dataGrid_Orders.SelectedItem;
                     string ID_List_of_order_items = (dataGrid_Orders.SelectedCells[0].Column.GetCellContent(item) as TextBlock).Text;
-                    MessageBox.Show(ID_List_of_order_items);
+                    //MessageBox.Show(ID_List_of_order_items);
+                    Debug.WriteLine(ID_List_of_order_items);
 
                     // Загрузка данных в dataGrid_List_of_order_items
                     using (OrdersdbEntities db = new OrdersdbEntities())
                     {
-                        var data =
-                                                from o in db.Orders.AsEnumerable()
-                                                from l in db.List_of_order_items
-                                                from p in db.Products
-                                                where o.orders_id == Convert.ToInt32(ID_List_of_order_items)
-                                                where l.item_product_id == o.list_of_order_items
-                                                where l.item_product_id == p.product_id
-                                                //group new { db.Orders, db.List_of_order_items, db.Products, l.item_count, p.product_price }
-                                                //    by new { o.orders_id, o.date, o.name_client } into g
-                                                select new
-                                                {
-                                                    product_id = p.product_id,
-                                                    product_name = p.product_name
-                                                };
+                       var dataFromQuary = from o in db.Orders.AsEnumerable()
+                                           from l in db.List_of_order_items
+                                           from p in db.Products
+                                           where o.orders_id == Convert.ToInt32(ID_List_of_order_items)
+                                           where l.list_of_order_items__id == o.list_of_order_items
+                                           where l.item_product_id == p.product_id
+                                           select new
+                                           {
+                                               product_id = p.product_id,
+                                               product_name = p.product_name,
+                                               item_count = l.item_count,
+                                               product_price = p.product_price
+                                           };
 
-                        dataGrid_List_of_order_items.ItemsSource = data;
-
-
+                        dataGrid_List_of_order_items.ItemsSource = dataFromQuary;
                     }
-    
-
-
-
-                }
-
-
-                
- 
+                }                              
             }
             catch (Exception e2)
             {
                 MessageBox.Show(e2.Message, "dataGrid_Orders_MouseUp", MessageBoxButton.OK, MessageBoxImage.Error);
-            }
-            
+            }            
+        }
+
+        private void CreateOrder_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(sender.ToString());
+            CreateOrder createOrder = new CreateOrder();
+
+
+            createOrder.ShowDialog();
+
+        }
+
+        private void EditOrder_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(sender.ToString());
+
+        }
+
+        private void DeleteOrder_Button_Click(object sender, RoutedEventArgs e)
+        {
+            //MessageBox.Show(sender.ToString());
 
         }
     }
